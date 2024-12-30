e08922873b43f2a261ad0a98906d3ca97f43a9c4
Adam <adam.wyatt@iinet.net.au>
2024-12-30 15:22:21 +0800

    team mapping id's

--- cmd/utils.go
+++ cmd/utils.go
@@ -23,6 +23,7 @@ var teamMapping = map[string]string{
 	"CRY": "Crystal Palace",
 	"EVE": "Everton",
 	"FUL": "Fulham",
+	"LEE": "Leeds",
 	"LEI": "Leicester City",
 	"LIV": "Liverpool",
 	"LUT": "Luton",
@@ -35,8 +36,38 @@ var teamMapping = map[string]string{
 	"SOU": "Southampton",
 	"TOT": "Tottenham Hotspur",
 	"WAT": "Watford",
+	"WBR": "West Brom",
 	"WHU": "West Ham",
 	"WOL": "Wolves",
+}
+
+var teamIDMapping = map[string]int{
+	"ARS": 42,   // Arsenal
+	"AVL": 66,   // Aston Villa
+	"BOU": 35,   // Bournemouth
+	"BRE": 55,   // Brentford
+	"BHA": 51,   // Brighton
+	"BUR": 44,   // Burnley
+	"CHE": 49,   // Chelsea
+	"CRY": 52,   // Crystal Palace
+	"EVE": 45,   // Everton
+	"FUL": 36,   // Fulham
+	"LEE": 63,   // Leeds
+	"LEI": 46,   // Leicester
+	"LIV": 40,   // Liverpool
+	"LUT": 1359, // Luton
+	"MCI": 50,   // Manchester City
+	"MUN": 33,   // Manchester United
+	"NEW": 34,   // Newcastle
+	"NOR": 71,   // Norwich
+	"NOT": 65,   // Nottingham Forest
+	"SHU": 62,   // Sheffield United
+	"SOU": 41,   // Southampton
+	"TOT": 47,   // Tottenham
+	"WAT": 38,   // Watford
+	"WBR": 60,   // West Brom
+	"WHU": 48,   // West Ham
+	"WOL": 39,   // Wolves
 }
 
 var (
